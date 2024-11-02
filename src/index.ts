9d1e0b1c49bef269a2fe597e249ceb8dac59637a
thomasxiaodongwu <755147511@qq.com>
2024-11-02 17:14:18 +0800

    add solana

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ async function getWalletBalance() {
 
 async function processToken(token: any): Promise<void> {
     return new Promise(async (resolve, reject) => {
-        let initAmount = 10000000; //0.01 SOL
+        let initAmount = 50000000; //0.05 SOL
         while (true) {
             const baseAmount = await getWalletBalance();
             const baseWAmount = await getWSOLBalance();
